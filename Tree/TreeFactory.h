3175bbee1455fc85cd844e35952a9d75aa68fd33
Jakub Banaszewski <bananow@gmail.com>
2014-02-25 00:49:14 +0100

    OPEN - task 5: Comments http://github.com/bejnan/MTFMag/issues/issue/5

--- Tree/TreeFactory.h
+++ Tree/TreeFactory.h
@@ -14,14 +14,21 @@
 using Base::Element;
 namespace Tree {
 
+/*
+ * Class to generate elements of appropriate type.
+ */
 class TreeFactory {
  public:
+  // root - Root of the tree to with elements will be added.
   TreeFactory(TreeRoot& root);
   virtual ~TreeFactory();
+
+  // Template method. Implemented for Element class and all its successors.
+  // id - unique identifier
   template<class T>
   void AddElement(int id);
   void DeleteElement(int id);
- private :
+ private:
   TreeRoot& root_;
 };
 
--- Tree/TreeRoot.h
+++ Tree/TreeRoot.h
@@ -19,19 +19,31 @@ using std::shared_ptr;
 
 namespace Tree {
 
+// Documentation and implementation in files TreeFactory.h and TreeFactory.cpp
 class TreeFactory;
 
+/*
+ * Class to manage tree of elements.
+ */
 class TreeRoot {
+  // Give access to private field to add elements.
   friend class TreeFactory;
  public:
   TreeRoot();
   virtual ~TreeRoot();
+  // Notify element with given id.
+  // After notification it is moved up to top of the tree.
   void NotifyContent(int id);
+  // Gives current position of element in hierarchy.
   int GetContentPosition(int id);
  protected:
+  // Moves element to top of the tree.
   void MoveElement(int position);
+  // Sort elements in each level by its value.
+  // Method used to refresh sorted_content_list.
   void SortElements();
  private:
+  // Tree implemented by array (parent is on position child div 2)
   vector<shared_ptr<Base::Element> > tree_list_;
   vector<int> sorted_content_list_;
   map<int, int> id_position_;
