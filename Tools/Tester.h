e6408f56d8e9993a48de316895e30f364525be62
Jakub Banaszewski <bananow@gmail.com>
2014-02-25 15:19:38 +0100

    close #5 - task 5: Comments http://github.com/bejnan/MTFMag/issues/issue/5

--- Tools/Tester.h
+++ Tools/Tester.h
@@ -14,6 +14,8 @@ namespace Tools {
  */
 class Tester {
  public:
+  // first_page_list_size - list of elements with no penalty
+  // second_page_list_size - list of elements with counted penalty
   Tester(int first_page_list_size, int second_page_list_size);
   virtual ~Tester();
   virtual void CountPenalty(unsigned int element_position);
@@ -21,7 +23,9 @@ class Tester {
  private:
   unsigned int first_page_list_size_;
   unsigned int second_page_list_size_;
+  // Const penalty for elements more distant than first and second page list
   unsigned const int MAX_PENALTY;
+  // Sum of penalties
   unsigned long penalty_;
 };
 
