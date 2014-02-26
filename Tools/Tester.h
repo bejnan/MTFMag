02668e1e337910176690cff8871dc8f5b41a0bbe
Jakub Banaszewski <bananow@gmail.com>
2014-02-26 08:05:26 +0100

    close #5

--- Tools/Tester.h
+++ Tools/Tester.h
@@ -18,6 +18,9 @@ class Tester {
   // second_page_list_size - list of elements with counted penalty
   Tester(int first_page_list_size, int second_page_list_size);
   virtual ~Tester();
+  // As written before, if element_position is less then first_page_list_size, no penalty is added.
+  // If element is on second_page_list_size, penalty is position - first_page_list_size.
+  // If element_position is more than two pages then MAX_PENALTY is added.
   virtual void CountPenalty(unsigned int element_position);
   virtual int penalty() { return penalty_; }
  private:
