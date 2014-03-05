64a26e964c2edd23db3c05a5c4407e74848eef3b
Jakub Banaszewski <bananow@gmail.com>
2014-03-05 12:49:22 +0100

    OPEN - task 10: unit tests! http://github.com/bejnan/MTFMag/issues/issue/10

--- Tree/TreeFactory_test.cpp
+++ Tree/TreeFactory_test.cpp
@@ -7,7 +7,18 @@
 
 #include "TreeFactory.h"
 #include "TreeRoot.h"
+#include "RandomTreeRoot.h"
 #include "../Headers/Elements.h"
+
+#include <vector>
+#include <algorithm>
+#include <random>
+
+using std::default_random_engine;
+using std::uniform_int_distribution;
+using std::find;
+using std::vector;
+
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
@@ -61,6 +72,86 @@ BOOST_AUTO_TEST_CASE(NotifyTest) {
   BOOST_CHECK_EQUAL(root.GetContentPosition(3), 2);
   BOOST_CHECK_EQUAL(root.GetContentPosition(4), 1);
   BOOST_CHECK_EQUAL(root.GetContentPosition(1), 3);
+
+}
+
+BOOST_AUTO_TEST_CASE(RandomTreeTest) {
+  Tree::RandomTreeRoot r_root;
+  Tree::TreeFactory tree_factory(r_root);
+
+  for (int i = 1; i < 10; i++)
+    tree_factory.AddElement<Base::SimpleElement>(i);
+
+  BOOST_CHECK_EQUAL(r_root.GetContentPosition(1), 1);
+  BOOST_CHECK_EQUAL(r_root.GetContentPosition(3), 3);
+
+  r_root.NotifyContent(1);
+  r_root.NotifyContent(4);
+  r_root.NotifyContent(7);
+  r_root.NotifyContent(9);
+
+  vector<int> positions;
+
+  for (int i = 1; i < 10; i++) {
+    positions.push_back(r_root.GetContentPosition(i));
+  }
+  for (int i = 1; i < 10; i++) {
+    BOOST_CHECK_EQUAL(
+        true, find(positions.begin(), positions.end(), i) != positions.end());
+  }
+
+}
+
+BOOST_AUTO_TEST_CASE(TreeTest) {
+  const int TEST_LIMIT = 20;
+  Tree::TreeRoot root;
+  Tree::TreeFactory tree_factory(root);
+  default_random_engine generator;
+  uniform_int_distribution<int> distribution(1, TEST_LIMIT);
+  for (int i = 1; i < TEST_LIMIT; i++)
+    tree_factory.AddElement<Base::SimpleElement>(i);
+
+  BOOST_CHECK_EQUAL(root.GetContentPosition(1), 1);
+  BOOST_CHECK_EQUAL(root.GetContentPosition(3), 3);
+
+  for (int i = 0; i < TEST_LIMIT*TEST_LIMIT; i++) {
+    root.NotifyContent(distribution(generator));
+  }
+  vector<int> positions;
+
+  for (int i = 1; i < TEST_LIMIT; i++) {
+    positions.push_back(root.GetContentPosition(i));
+  }
+  for (int i = 1; i < TEST_LIMIT; i++) {
+    BOOST_CHECK_EQUAL(
+        true, find(positions.begin(), positions.end(), i) != positions.end());
+  }
+
+}
+BOOST_AUTO_TEST_CASE(RandomTreeTest2) {
+  const int TEST_LIMIT = 20;
+  Tree::RandomTreeRoot root;
+  Tree::TreeFactory tree_factory(root);
+  default_random_engine generator;
+  uniform_int_distribution<int> distribution(1, TEST_LIMIT);
+  for (int i = 1; i < TEST_LIMIT; i++)
+    tree_factory.AddElement<Base::SimpleElement>(i);
+
+  BOOST_CHECK_EQUAL(root.GetContentPosition(1), 1);
+  BOOST_CHECK_EQUAL(root.GetContentPosition(3), 3);
+
+  for (int i = 0; i < TEST_LIMIT*TEST_LIMIT; i++) {
+    root.NotifyContent(distribution(generator));
+  }
+  vector<int> positions;
+
+  for (int i = 1; i < TEST_LIMIT; i++) {
+    positions.push_back(root.GetContentPosition(i));
+  }
+  for (int i = 1; i < TEST_LIMIT; i++) {
+    BOOST_CHECK_EQUAL(
+        true, find(positions.begin(), positions.end(), i) != positions.end());
+  }
 
 }
 
