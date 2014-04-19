0b46a47dce4549d9494c63f59ed8499bd8b6cca1
kuba <kuba@localhost.localdomain>
2014-04-20 01:54:22 +0200

    OPEN - task 18: Documentation!! http://github.com/bejnan/MTFMag/issues/issue/18 Element classes upgrade

---
+++ Doc/html/classBase_1_1ComplexDifferenceElement.js
@@ -0,0 +1,9 @@
+var classBase_1_1ComplexDifferenceElement =
+[
+    [ "ComplexDifferenceElement", "classBase_1_1ComplexDifferenceElement.html#ad8b835528b8b5a6051c6405af99e48f3", null ],
+    [ "~ComplexDifferenceElement", "classBase_1_1ComplexDifferenceElement.html#a02193b0a599099bdbcb85f96ff5c404b", null ],
+    [ "Clone", "classBase_1_1ComplexDifferenceElement.html#a6aeeb7eac0b145c8a000aa59369254cd", null ],
+    [ "Compare", "classBase_1_1ComplexDifferenceElement.html#a31787b2362f6970a6cdb4ff21cf3e01e", null ],
+    [ "Difference", "classBase_1_1ComplexDifferenceElement.html#a0587d9e21c012c8c424f5ac5c5a5328f", null ],
+    [ "Notify", "classBase_1_1ComplexDifferenceElement.html#a172e4e7223c50f84b5f2f8fdd52ea8d0", null ]
+];
---
+++ Doc/html/classBase_1_1SimpleElement.js
@@ -0,0 +1,9 @@
+var classBase_1_1SimpleElement =
+[
+    [ "SimpleElement", "classBase_1_1SimpleElement.html#a90848472a91d3827f2e32e04b345500b", null ],
+    [ "~SimpleElement", "classBase_1_1SimpleElement.html#a4580b6f8e363c12459a2947749c26226", null ],
+    [ "Clone", "classBase_1_1SimpleElement.html#a37f8f62ac37af39a0fcde45f67c92ee0", null ],
+    [ "Compare", "classBase_1_1SimpleElement.html#ae798ef9d47ac065392dea25d07f6970e", null ],
+    [ "Difference", "classBase_1_1SimpleElement.html#a8f7d8fc5d5604f3253ff2e314dfd1cf5", null ],
+    [ "Notify", "classBase_1_1SimpleElement.html#aca55505bef5c6aa5b9b36051a3930ba0", null ]
+];
