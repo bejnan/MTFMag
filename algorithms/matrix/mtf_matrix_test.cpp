3d031723f9c8823b59ac385d2500a6894663bd85
Jakub Banaszewski <bananow@gmail.com>
2014-05-01 15:34:55 +0200

    changes in tests (Base::Element compare change influenced element order)

--- algorithms/matrix/mtf_matrix_test.cpp
+++ algorithms/matrix/mtf_matrix_test.cpp
@@ -29,7 +29,9 @@ BOOST_AUTO_TEST_CASE(AddingElement) {
 BOOST_AUTO_TEST_CASE(MTFMatrixNotifiaction) {
   //initialization
   shared_ptr<MatrixBuilder> builder = MatrixBuilder::GetInstance();
-  shared_ptr<MTFMatrix> matrix = builder->WithMatrixWidth(2).Generate();
+  shared_ptr<MTFMatrix> matrix =
+      builder->WithMatrixWidth(2).SetElementPrototype(
+          Base::Element::GetPrototype()).Generate();
 
   shared_ptr<Base::Element> element;
   for (int i = 1; i < 6; i++) {
@@ -37,17 +39,21 @@ BOOST_AUTO_TEST_CASE(MTFMatrixNotifiaction) {
   }
 
   matrix->NotifyContent(2);
+  //changes in order is because in Base::Element id number
+  //influence compare
   BOOST_CHECK_EQUAL(1, matrix->GetContentPosition(2));
   BOOST_CHECK_EQUAL(2, matrix->GetContentPosition(1));
-  BOOST_CHECK_EQUAL(3, matrix->GetContentPosition(3));
-  BOOST_CHECK_EQUAL(4, matrix->GetContentPosition(4));
+  BOOST_CHECK_EQUAL(4, matrix->GetContentPosition(3));
+  BOOST_CHECK_EQUAL(3, matrix->GetContentPosition(4));
   BOOST_CHECK_EQUAL(5, matrix->GetContentPosition(5));
 }
 
 BOOST_AUTO_TEST_CASE(MTFMatrixTwoNotifiactions) {
   //initialization
   shared_ptr<MatrixBuilder> builder = MatrixBuilder::GetInstance();
-  shared_ptr<MTFMatrix> matrix = builder->WithMatrixWidth(2).Generate();
+  shared_ptr<MTFMatrix> matrix =
+      builder->WithMatrixWidth(2).SetElementPrototype(
+          Base::Element::GetPrototype()).Generate();
 
   shared_ptr<Base::Element> element;
   for (int i = 1; i < 6; i++) {
@@ -55,18 +61,20 @@ BOOST_AUTO_TEST_CASE(MTFMatrixTwoNotifiactions) {
   }
 
   matrix->NotifyContent(2);
+  //changes in order is because in Base::Element id number
+  //influence comparison
   BOOST_CHECK_EQUAL(1, matrix->GetContentPosition(2));
   BOOST_CHECK_EQUAL(2, matrix->GetContentPosition(1));
-  BOOST_CHECK_EQUAL(3, matrix->GetContentPosition(3));
-  BOOST_CHECK_EQUAL(4, matrix->GetContentPosition(4));
+  BOOST_CHECK_EQUAL(4, matrix->GetContentPosition(3));
+  BOOST_CHECK_EQUAL(3, matrix->GetContentPosition(4));
   BOOST_CHECK_EQUAL(5, matrix->GetContentPosition(5));
 
   // last element become first
   matrix->NotifyContent(5);
   BOOST_CHECK_EQUAL(1, matrix->GetContentPosition(5));
-  BOOST_CHECK_EQUAL(2, matrix->GetContentPosition(1));
-  BOOST_CHECK_EQUAL(3, matrix->GetContentPosition(2));
-  BOOST_CHECK_EQUAL(4, matrix->GetContentPosition(4));
+  BOOST_CHECK_EQUAL(2, matrix->GetContentPosition(2));
+  BOOST_CHECK_EQUAL(4, matrix->GetContentPosition(1));
+  BOOST_CHECK_EQUAL(3, matrix->GetContentPosition(4));
   BOOST_CHECK_EQUAL(5, matrix->GetContentPosition(3));
 
 }
