856526dc439d333a15c510e692b738452977b5b8
Jakub Banaszewski <bananow@gmail.com>
2014-06-07 17:12:28 +0200

    34: Add missing tests Doc update Task-Url: http://github.com/bejnan/MTFMag/issues/issue/34

--- Doc/html/annotated.js
+++ Doc/html/annotated.js
@@ -2,36 +2,36 @@ var annotated =
 [
     [ "Algorithms", null, [
       [ "Algorithm", "classAlgorithms_1_1Algorithm.html", "classAlgorithms_1_1Algorithm" ],
-      [ "MTFMatrix", "classAlgorithms_1_1MTFMatrix.html", "classAlgorithms_1_1MTFMatrix" ],
       [ "MoveToFront", "classAlgorithms_1_1MoveToFront.html", "classAlgorithms_1_1MoveToFront" ],
+      [ "MTFMatrix", "classAlgorithms_1_1MTFMatrix.html", "classAlgorithms_1_1MTFMatrix" ],
       [ "RandomTreeRoot", "classAlgorithms_1_1RandomTreeRoot.html", "classAlgorithms_1_1RandomTreeRoot" ],
       [ "TreeRoot", "classAlgorithms_1_1TreeRoot.html", "classAlgorithms_1_1TreeRoot" ]
     ] ],
     [ "Base", null, [
+      [ "ComplexDifferenceElement", "classBase_1_1ComplexDifferenceElement.html", "classBase_1_1ComplexDifferenceElement" ],
       [ "Configuration", "classBase_1_1Configuration.html", "classBase_1_1Configuration" ],
       [ "ConsoleProgramRunner", "classBase_1_1ConsoleProgramRunner.html", "classBase_1_1ConsoleProgramRunner" ],
-      [ "FileProgramRunner", "classBase_1_1FileProgramRunner.html", "classBase_1_1FileProgramRunner" ],
-      [ "ProgramRunner", "classBase_1_1ProgramRunner.html", "classBase_1_1ProgramRunner" ],
       [ "CsvDataOutput", "classBase_1_1CsvDataOutput.html", "classBase_1_1CsvDataOutput" ],
       [ "CsvFileDataOutput", "classBase_1_1CsvFileDataOutput.html", "classBase_1_1CsvFileDataOutput" ],
+      [ "Database", "classBase_1_1Database.html", "classBase_1_1Database" ],
+      [ "DataCollector", "classBase_1_1DataCollector.html", "classBase_1_1DataCollector" ],
       [ "DataOutput", "classBase_1_1DataOutput.html", "classBase_1_1DataOutput" ],
       [ "DataOutputBuilder", "classBase_1_1DataOutputBuilder.html", "classBase_1_1DataOutputBuilder" ],
-      [ "FileDataOutput", "classBase_1_1FileDataOutput.html", "classBase_1_1FileDataOutput" ],
       [ "DataProvider", "classBase_1_1DataProvider.html", "classBase_1_1DataProvider" ],
-      [ "FileDataProvider", "classBase_1_1FileDataProvider.html", "classBase_1_1FileDataProvider" ],
-      [ "Database", "classBase_1_1Database.html", "classBase_1_1Database" ],
-      [ "OverallResult", "classBase_1_1OverallResult.html", "classBase_1_1OverallResult" ],
-      [ "Result", "classBase_1_1Result.html", "classBase_1_1Result" ],
-      [ "DataCollector", "classBase_1_1DataCollector.html", "classBase_1_1DataCollector" ],
-      [ "JudgeCollector", "classBase_1_1JudgeCollector.html", "classBase_1_1JudgeCollector" ],
-      [ "ResultCollector", "classBase_1_1ResultCollector.html", "classBase_1_1ResultCollector" ],
-      [ "ComplexDifferenceElement", "classBase_1_1ComplexDifferenceElement.html", "classBase_1_1ComplexDifferenceElement" ],
       [ "Element", "classBase_1_1Element.html", "classBase_1_1Element" ],
+      [ "FileDataOutput", "classBase_1_1FileDataOutput.html", "classBase_1_1FileDataOutput" ],
+      [ "FileDataProvider", "classBase_1_1FileDataProvider.html", "classBase_1_1FileDataProvider" ],
+      [ "FileProgramRunner", "classBase_1_1FileProgramRunner.html", "classBase_1_1FileProgramRunner" ],
+      [ "JudgeCollector", "classBase_1_1JudgeCollector.html", "classBase_1_1JudgeCollector" ],
+      [ "OverallResult", "classBase_1_1OverallResult.html", "classBase_1_1OverallResult" ],
+      [ "ProgramRunner", "classBase_1_1ProgramRunner.html", "classBase_1_1ProgramRunner" ],
+      [ "Result", "classBase_1_1Result.html", "classBase_1_1Result" ],
+      [ "ResultCollector", "classBase_1_1ResultCollector.html", "classBase_1_1ResultCollector" ],
       [ "SimpleElement", "classBase_1_1SimpleElement.html", "classBase_1_1SimpleElement" ]
     ] ],
     [ "Exception", null, [
-      [ "DataProviderException", "classException_1_1DataProviderException.html", null ],
       [ "DatabaseException", "classException_1_1DatabaseException.html", null ],
+      [ "DataProviderException", "classException_1_1DataProviderException.html", null ],
       [ "EndOfFileException", "classException_1_1EndOfFileException.html", null ],
       [ "FilePathException", "classException_1_1FilePathException.html", "classException_1_1FilePathException" ],
       [ "InvalidIndexException", "classException_1_1InvalidIndexException.html", "classException_1_1InvalidIndexException" ],
@@ -39,8 +39,8 @@ var annotated =
     ] ],
     [ "Tools", null, [
       [ "Judge", "classTools_1_1Judge.html", "classTools_1_1Judge" ],
-      [ "Tester", "classTools_1_1Tester.html", "classTools_1_1Tester" ],
       [ "Processor", "classTools_1_1Processor.html", "classTools_1_1Processor" ],
-      [ "ProcessorFactory", "classTools_1_1ProcessorFactory.html", "classTools_1_1ProcessorFactory" ]
+      [ "ProcessorFactory", "classTools_1_1ProcessorFactory.html", "classTools_1_1ProcessorFactory" ],
+      [ "Tester", "classTools_1_1Tester.html", "classTools_1_1Tester" ]
     ] ]
 ];
